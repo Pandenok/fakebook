f2f4c2f0afbe599a4d398f8081bd01ba945e26e0
pandenok <dimitri.shatunin@gmail.com>
2020-12-05 23:31:26 +0100

    Create Comment model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,5 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_134639) do
+ActiveRecord::Schema.define(version: 2020_12_05_222552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "friend_requests", force: :cascade do |t|
     t.integer "user_id"
@@ -56,6 +66,8 @@ ActiveRecord::Schema.define(version: 2020_12_04_134639) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
