e172fff2434baa1519e726ad1c178e0842b382c5
pandenok <dimitri.shatunin@gmail.com>
2020-12-06 22:34:33 +0100

    Nest the comment resources inside the post resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :friend_requests, only: [:create, :update, :destroy]
   resources :posts do
     resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
 end
