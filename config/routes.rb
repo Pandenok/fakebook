9dea1441bb3b08b93847909fad5c905e740781ce
pandenok <dimitri.shatunin@gmail.com>
2020-12-10 01:08:27 +0100

    Change root to Posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'static#index'
+  root to: 'posts#index'
   resources :friend_requests, only: [:create, :update, :destroy]
   resources :posts do
     resources :likes, only: [:create, :destroy]
