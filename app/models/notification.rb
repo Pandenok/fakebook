5ec1bed96a44d7008057414cffa7c17bf3c8f13e
pandenok <dimitri.shatunin@gmail.com>
2020-12-13 00:47:21 +0100

    Show only recent unseen notifications in dropdown

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,4 +3,6 @@ class Notification < ApplicationRecord
   belongs_to :sent_by, class_name: 'User'
   belongs_to :notifiable, polymorphic: true
   enum status: { seen: 'Seen', unseen: 'Unseen' }
+  scope :unseen, -> { where(status: :unseen) }
+  scope :recent, -> { order(created_at: :desc) }
 end
