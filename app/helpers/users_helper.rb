7a53b62132fd67d206b47b1540996795ffc4a551
pandenok <dimitri.shatunin@gmail.com>
2020-12-18 01:11:08 +0100

    Display avatar photo on user's profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@ module UsersHelper
   private
 
   def user_params
-    params.require(:user).permit(:cover_photo)
+    params.require(:user).permit(:cover_photo, :avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,5 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :notifications, foreign_key: :sent_to_id, dependent: :destroy
   has_one_attached :cover_photo
+  has_one_attached :avatar
 end
