aea8bbc0635ee68c655c98338fa3dc67179b7cb5
pandenok <dimitri.shatunin@gmail.com>
2020-12-18 23:50:20 +0100

    Set a default avatar for user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def avatar_for(user)
+    if user.avatar.attached?
+      user.avatar
+    elsif user.male?
+      "male_default.png"
+    elsif user.female?
+      "female_default.png"
+    end
+  end
 end
