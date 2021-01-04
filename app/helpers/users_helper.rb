673b9628e99c001479adaaaaab009cea1f8baf13
pandenok <dimitri.shatunin@gmail.com>
2021-01-04 19:31:09 +0100

    Whitelist new user params

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,13 @@ module UsersHelper
   private
 
   def user_params
-    params.require(:user).permit(:cover_photo, :avatar)
+    params.require(:user)
+          .permit(:cover_photo,
+                  :avatar,
+                  :bio,
+                  :workplace,
+                  :hometown,
+                  :relationship_status,
+                  :hobbies)
   end
 end
